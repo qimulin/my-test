9a35b3d75279543feeb97e446b4085cbd284cde2
linxichun <linxichun@chuangjiangx.com>
2019-08-22 19:03:19 +0800

    mytest1提交2

--- src/main/java/zhou/wu/mytest/test/MyTest.java
+++ src/main/java/zhou/wu/mytest/test/MyTest.java
@@ -10,9 +10,9 @@ public class MyTest {
 
     public static void main(String[] args) {
         Map<String,String> testMap = new HashMap<>();
-        testMap.put("1","1");
-        testMap.put("2","2");
-        testMap.put("3","3");
+        testMap.put("1","21");
+        testMap.put("2","22");
+        testMap.put("3","23");
         Map<String,String> testMap1 = testMap;
         removeField(testMap1);
         System.out.println(testMap.size());
