a975e79486d81e5eae8a1ddf9875f006dcac9202
linxichun <linxichun@citydo.com.cn>
2020-12-19 14:32:29 +0800

    调整接口写法

--- src/main/java/zhou/wu/mytest/lambda/lazy_execution/LambdaLazyMain.java
+++ src/main/java/zhou/wu/mytest/lambda/lazy_execution/LambdaLazyMain.java
@@ -2,6 +2,8 @@ package zhou.wu.mytest.lambda.lazy_execution;
 
 import org.junit.Test;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by lin.xc on 2019/7/19
  */
@@ -33,6 +35,16 @@ public class LambdaLazyMain {
         if (level == 1) {//只有当条件满足才会执行Lambda表达式中的拼接
             System.out.println(builder.buildMessage());
         }
+    }
+
+    public static void main(String[] args) {
+//        double auxiliaryValue = Math.pow(10, 2);
+//        double auxiliaryValue = 200.01;
+//        String auxiliaryValueStr = new DecimalFormat("0").format(auxiliaryValue);
+//        Double aDouble = Double.valueOf(auxiliaryValue);
+        Integer auxiliaryValue = Double.valueOf(Math.pow(10, 3)).intValue();
+        String result = String.format("CEIL(%s*%d)/%d","c1",auxiliaryValue,auxiliaryValue);
+        System.out.println(result);
     }
 }
 
--- src/main/java/zhou/wu/mytest/web/controller/UserInfoController.java
+++ src/main/java/zhou/wu/mytest/web/controller/UserInfoController.java
@@ -1,6 +1,5 @@
 package zhou.wu.mytest.web.controller;
 
-import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -21,7 +20,15 @@ public class UserInfoController {
     private UserInfoService userInfoService;
 
     @PostMapping
-    @DemoAnnotation(value = "insertUserInfo", primaryId = "#userInfo.userage")
+    @GetMapping("/by-user-no")
+    public AutoUserInfo findUserInfo(String userNo){
+        // 根据userNo查询
+        AutoUserInfo autoUserInfo = userInfoService.selectByUserNo(userNo);
+        return autoUserInfo;
+    }
+
+    @PostMapping("/add")
+//    @DemoAnnotation(value = "insertUserInfo", primaryId = "#userInfo.userage")
     public AutoUserInfo insertUserInfo(@RequestBody AutoUserInfo userInfo){
         AutoUserInfo autoUserInfo = userInfoService.selectByUserNo(userInfo.getUserno());
 //        if(autoUserInfo==null){
