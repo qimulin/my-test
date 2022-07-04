652140b690a05915a224bd0e383cb099da15d4cf
linxc <582192258@qq.com>
2022-07-04 14:15:42 +0800

    Scala环境Hello类提交

---
+++ modules/boot-spark/src/main/java/zhou/wu/boot/spark/core/HelloScala.scala
@@ -0,0 +1,12 @@
+package zhou.wu.boot.spark.core
+
+/**
+ * @description: Scala体验
+ * @author zhou.wu
+ * @date 2022/7/4
+ * */
+object HelloScala {
+  def main(args: Array[String]): Unit = {
+    println("hello scala")
+  }
+}
