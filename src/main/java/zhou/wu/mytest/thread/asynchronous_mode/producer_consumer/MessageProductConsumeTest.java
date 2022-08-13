2230c98035ded732eafb7044bfefd4ac1ddf6629
linxc <582192258@qq.com>
2022-08-13 17:25:35 +0800

    异步模式之生产消费模式

---
+++ src/main/java/zhou/wu/mytest/thread/asynchronous_mode/producer_consumer/MessageProductConsumeTest.java
@@ -0,0 +1,42 @@
+package zhou.wu.mytest.thread.asynchronous_mode.producer_consumer;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @author zhou.wu
+ * @description: 消息试生产消费示例
+ * @date 2022/8/13
+ **/
+@Slf4j
+public class MessageProductConsumeTest {
+
+    public static void main(String[] args) {
+        MessageQueue messageQueue = new MessageQueue(2);
+        // 4 个生产者线程, 下载任务
+        for (int i = 0; i < 4; i++) {
+            int id = i;
+            new Thread(() -> {
+                try {
+                    log.debug("download...");
+                    List<String> response = Downloader.download();
+                    log.debug("try put message({})", id);
+                    messageQueue.put(new Message(id, response));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }, "生产者" + i).start();
+        }
+
+        // 1 个消费者线程, 处理结果
+        new Thread(() -> {
+            while (true) {
+                Message message = messageQueue.take();
+                List<String> response = (List<String>) message.getMessage();
+                log.debug("take message({}): [{}] lines", message.getId(), response.size());
+            }
+        }, "消费者").start();
+    }
+}
