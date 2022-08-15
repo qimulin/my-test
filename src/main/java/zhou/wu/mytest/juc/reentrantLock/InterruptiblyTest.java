43b9c86a1ec72c86b6511553da152896f190aaf2
linxc <582192258@qq.com>
2022-08-15 17:05:32 +0800

    Synchronized不可中断原因补充

--- src/main/java/zhou/wu/mytest/juc/reentrantLock/InterruptiblyTest.java
+++ src/main/java/zhou/wu/mytest/juc/reentrantLock/InterruptiblyTest.java
@@ -8,12 +8,23 @@ import java.util.concurrent.locks.ReentrantLock;
 /**
  * @author zhou.wu
  * @description: 可打断测试
+ * 为什么synchronized不能中断ReentrantLock能中断？
+ * ReentrantLock是可中断锁，而synchronized 不是可中断锁。ReentrantLock可以获取到锁状态的。
+ *
+ * 例如：线程A和B都要获取对象O的锁定，假设A获取了对象O锁，B将等待A释放对O的锁定
+ * - 如果使用 synchronized， 如果A不释放，B将一直阻塞下去，A不释放锁，B就一直不被中断（线程状态是Blocked的情况下无法被中断）
+ * - 如果使用 ReentrantLock，如果A不释放，可以使B在等待了足够长的时间以后，中断等待，而干别的事情
  * @date 2022/8/15
  **/
 @Slf4j
 public class InterruptiblyTest {
 
     public static void main(String[] args) {
+        useReentrantLock();
+//        useSynchronized();
+    }
+
+    private static void useReentrantLock(){
         ReentrantLock lock = new ReentrantLock();
         Thread t1 = new Thread(() -> {
             log.debug("启动...");
@@ -50,4 +61,44 @@ public class InterruptiblyTest {
             lock.unlock();
         }
     }
+
+    /**
+     * 竞争不到锁而处于BLOCKED状态的线程能否响应中断？
+     *
+     * 其一：由于处于BLOCKED状态只能通过异常机制来响应中断，能使线程进入BLOCKED状态的只有synchronized关键字。但是关键字是无法抛出异常的，方法才能抛出异常。
+     * 其二：一个线程不可能一直处于BLOCKED状态，不然就是线程饥饿或者线程死锁了，虽然BLOCKED无法响应中断，但是进入运行状态就可以感知到了，所以是没问题的。
+     *
+     * 自解：
+     * 1、InterruptedException是要识别在使用方法上的，synchronized是关键字，不能设置抛出这种异常；
+     * 2、BLOCKED状态本来就不不该是一个持久的状态，这个状态下本身就完全争抢不到时间片，所以结合第1点的原因，允许等到它转为其他可中断的状态时，再进行中断也行。
+     * */
+    private static void useSynchronized(){
+        Object lock = new Object();
+        Thread t1 = new Thread(() -> {
+            log.debug("启动...");
+
+            synchronized (lock){
+                long i = 1000000;
+                while(i-->0){
+                    String str = "doSomething";
+                }
+            }
+            log.debug("获得了锁");
+        }, "t1");
+
+
+        synchronized (lock){
+            log.debug("获得了锁");
+            t1.start();
+            try {
+                TimeUnit.SECONDS.sleep(3);
+                // 就算调用t1的interrupt方法，还是不能对t1进行打断，这点从t1的代码都没有提醒处理InterruptedException异常能看出，synchronized关键字目前并不能强制要求处理InterruptedException
+                t1.interrupt();
+                log.debug("执行打断");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
--- src/main/java/zhou/wu/mytest/thread/synchronize/Demo1.java
+++ src/main/java/zhou/wu/mytest/thread/synchronize/Demo1.java
@@ -1,23 +1,28 @@
 package zhou.wu.mytest.thread.synchronize;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author lin.xc
  * @date 2020/11/27
  **/
+@Slf4j
 public class Demo1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Demo1 demo1 = new Demo1();
         new Thread(() -> {
             demo1.syncMethod(Thread.currentThread().getName(), 1);
-            System.out.println(Thread.currentThread().getName()+"再次执行！");
+            log.debug(Thread.currentThread().getName()+"再次执行！");
             demo1.syncMethod(Thread.currentThread().getName(), 1);
         }).start();
 
-        new Thread(() -> demo1.syncMethod(Thread.currentThread().getName(), 2)).start();
+        Thread t1 = new Thread(() -> demo1.syncMethod(Thread.currentThread().getName(), 2), "t1");
+        Thread t2 = new Thread(() -> demo1.syncMethod(Thread.currentThread().getName(), 1), "t2");
+        Thread t3 = new Thread(() -> demo1.syncMethod(Thread.currentThread().getName(), 1), "t3");
 
-        new Thread(() -> demo1.syncMethod(Thread.currentThread().getName(), 1)).start();
-
-        new Thread(() -> demo1.syncMethod(Thread.currentThread().getName(), 1)).start();
+        t1.start();
+        t2.start();
+        t3.start();
     }
 
     /**
@@ -27,10 +32,11 @@ public class Demo1 {
         System.out.println("进入方法，参数："+threadName+"，"+flagId);
         synchronized(flagId){
             try {
-                System.out.println(threadName+" 执行同步方法需要5s");
+                log.debug(threadName+" 执行同步方法需要5s");
                 Thread.sleep(5000);
-                System.out.println(threadName+" 执行同步方法完毕");
+                log.debug(threadName+" 执行同步方法完毕");
             } catch (InterruptedException e) {
+                log.error(e.getMessage());
                 e.printStackTrace();
             }
         }
