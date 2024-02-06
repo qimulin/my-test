06fffed295568f348f6779dbbe27c9a886fb81ba
linxc <582192258@qq.com>
2024-02-06 16:32:23 +0800

    补充调试和说明

--- src/main/java/zhou/wu/mytest/guava/ratelimiter/SmoothBurstyDemo.java
+++ src/main/java/zhou/wu/mytest/guava/ratelimiter/SmoothBurstyDemo.java
@@ -1,6 +1,9 @@
 package zhou.wu.mytest.guava.ratelimiter;
 
 import com.google.common.util.concurrent.RateLimiter;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Guava的RateLimiter提供了令牌桶算法实现：平滑突发限流(SmoothBursty)和平滑预热限流(SmoothWarmingUp)实现。
@@ -8,12 +11,14 @@ import com.google.common.util.concurrent.RateLimiter;
  * @author zhou.wu
  * @date 2024/2/6
  **/
+@Slf4j
 public class SmoothBurstyDemo {
 
     public static void main(String[] args) {
 //        testSmoothBursty();
 //        testSmoothBursty2();
-        testSmoothBursty3();
+//        testSmoothBursty3();
+        testSmoothBursty4();
     }
 
     /**
@@ -91,6 +96,32 @@ public class SmoothBurstyDemo {
              * get 1 tokens: 0.995625s 滞后效应，需要替前一个请求进行等待
              * get 1 tokens: 0.194603s
              * get 1 tokens: 0.196866s
+             */
+        }
+    }
+
+    public static void testSmoothBursty4() {
+        RateLimiter r = RateLimiter.create(1);
+        while (true)
+        {
+            /*
+            * acquire 方法用于从令牌桶中获取令牌。它的参数表示请求的令牌数量，方法会根据令牌桶中的可用令牌数量和请求的令牌数量来确定是否需要等待以及等待的时间。
+            * 如果令牌桶中有足够的令牌，acquire 方法会立即返回，并且消耗相应数量的令牌；否则，它会等待直到令牌桶中有足够的令牌后再返回
+            * */
+            System.out.println("get 5 tokens: " + r.acquire(5) + "s");
+            System.out.println("end");
+            /**
+             * output:
+             * get 5 tokens: 0.0s 为什么第一次获取5个的时候，不需要等令牌桶放满？因为：
+             * 实际上，令牌桶算法允许在刚开始时就允许一次性请求所有令牌。这是因为令牌桶算法的关键特性之一是它的灵活性。在刚开始时，如果允许一次性请求所有令牌，可以避免在系统刚启动时因为请求而产生的等待时间，从而提高系统的响应性能。
+             * 自解：简单总结，就是刚启动避免等待，允许任何数量的执行
+             * end
+             * get 5 tokens: 4.984725s 非第一次，都会等令牌桶放满5个，再给出
+             * end
+             * get 5 tokens: 4.996115s
+             * end
+             * get 5 tokens: 4.999534s
+             * end
              */
         }
     }
