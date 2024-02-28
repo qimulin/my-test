710a1eb9718f4253965f7a74ddc3129fc2568567
linxc <582192258@qq.com>
2024-02-28 18:09:16 +0800

    对单独提取boot-web模块后的my-test删除相关文件和依赖

--- src/main/java/zhou/wu/mytest/web/mapper/dal/AutoUserInfoDalMapper.java
+++
@@ -1,8 +0,0 @@
-package zhou.wu.mytest.web.mapper.dal;
-
-import zhou.wu.mytest.web.domain.AutoUserInfo;
-
-public interface AutoUserInfoDalMapper {
-
-    AutoUserInfo getByUserNo(String userNo);
-}
